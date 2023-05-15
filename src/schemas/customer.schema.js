422cf6412396e0aeafdf5a2d8f5afd1ad452f167
FernandoM52 <fernandomartins.contato@outlook.com>
2023-05-15 15:56:07 -0300

    refactor/endpoint de cliente - Ajustado objeto do customerSchema e formato do objeto de respsota de um cliente específico

--- src/schemas/customer.schema.js
+++ src/schemas/customer.schema.js
@@ -5,5 +5,5 @@ export const customerSchema = joi.object({
   phone: joi.string().pattern(/^[0-9]+$/, "numbers").min(10).max(11)
     .required(),
   cpf: joi.string().pattern(/^[0-9]+$/, "numbers").length(11).required(),
-  birthday: joi.string().isoDate().required(),
+  birthday: joi.string().required(),
 });
