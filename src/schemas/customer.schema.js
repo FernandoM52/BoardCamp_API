5db470f0761fc11f08cd0351fc3d26409c011c42
FernandoM52 <fernandomartins.contato@outlook.com>
2023-05-15 16:09:13 -0300

    refactor/endpoint de cliente - Ajustado customerSchema e fo… …rmato do objeto de resposta de um cliente específico

--- src/schemas/customer.schema.js
+++ src/schemas/customer.schema.js
@@ -2,8 +2,8 @@ import joi from "joi";
 
 export const customerSchema = joi.object({
   name: joi.string().required(),
-  phone: joi.string().pattern(/^[0-9]+$/, "numbers").min(10).max(11)
+  phone: joi.string().pattern(new RegExp('^\\d{11}$')).min(10).max(11)
     .required(),
   cpf: joi.string().pattern(/^[0-9]+$/, "numbers").length(11).required(),
-  birthday: joi.string().required(),
+  birthday: joi.date().max("now").iso().required(),
 });
